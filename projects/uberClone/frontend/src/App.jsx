8dd0c7613a19556151e211555d24c1b7e3709fa2
Sanket Mishra <sanketmishra@jerry.local>
2025-01-03 16:46:12 +0530

    Frontend initial setup.

---
+++ projects/uberClone/frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-slate-800 text-3xl font-serif underline'>App</h1>
+    </div>
+  )
+}
+
+export default App
